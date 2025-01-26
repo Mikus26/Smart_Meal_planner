b1f0a52d5818d263d1f13d4e37d084c9199d1633
mikus26 <m.walenciak@icloud.com>
2025-01-26 18:41:42 +0100

    front season form new et index

--- src/Form/settings/SeasonType.php
+++ src/Form/settings/SeasonType.php
@@ -12,7 +12,9 @@ class SeasonType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', null, [
+                'attr' => ['class' => 'form-control']
+            ])
         ;
     }
 
